a8896351348522231a71a953b0ee1062e92a9bd4
Jacob Wolf <jacob.h.wolf@gmail.com>
2019-08-15 17:09:40 +0800

    added rest of installs and file system setup to script

--- setup_script.sh
+++ setup_script.sh
@@ -3,42 +3,43 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
-MAGENTA='\033[0;164m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m]'
 NC='\033[0m'
 
-printf "${MAGENTA}Creating file system...${NC}\n"
+printf "${BLUE}Creating file system...${NC}\n"
 cd /Users/`whoami`/Desktop
-mkdir g9CS
+mkdir -p g9CS
 cd g9CS
-mkdir unit_00
-printf "${GREEN}✓✓✓ File system created!${NC}\n"
+mkdir -p unit_00
+printf "${GREEN}✓✓✓ File system created!${NC}\n\n\n"
 
-printf "${MAGENTA}Installing Homebrew...${NC}\n"
+printf "${BLUE}Installing Homebrew...${NC}\n"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#need response from user
-printf "${GREEN}✓✓✓ Homebrew installed!${NC}\n"
-#
-# printf "${MAGENTA}Installing Python3...${NC}\n"
-# brew install python3
-# printf "${GREEN}✓✓✓ Python3 installed!${NC}\n"
-#
-# printf "${MAGENTA}Installing git...${NC}\n"
-# brew install git
-# printf "${GREEN}✓✓✓ git installed!${NC}\n"
-#
-# printf "${MAGENTA}Installing direnv...${NC}\n"
-# brew install direnv
-# printf "${GREEN}✓✓✓ direnv installed!${NC}\n"
-#
-# printf "${MAGENTA}Installing Atom...${NC}\n"
-# brew cask install atom
-# printf "${GREEN}✓✓✓ Atom installed!${NC}\n"
-#
-# echo 'eval "$(direnv hook bash)"' > /Users/`whoami`/.bash_profile
-# #restart terminal?
-#
-# printf "${MAGENTA}Creating virtual environment...${NC}\n"
-# python3 -m venv env
-# echo 'source env/bin/activate' > .envrc
-# direnv allow .
-# printf "${GREEN}✓✓✓ virtual environment created! ${NC}\n"
+printf "${GREEN}✓✓✓ Homebrew installed!${NC}\n\n\n"
+
+printf "${BLUE}Installing Python3...${NC}\n"
+brew install python3
+printf "${GREEN}✓✓✓ Python3 installed!${NC}\n\n\n"
+
+printf "${BLUE}Installing git...${NC}\n"
+brew install git
+printf "${GREEN}✓✓✓ git installed!${NC}\n\n\n"
+
+printf "${BLUE}Installing direnv...${NC}\n"
+brew install direnv
+echo 'eval "$(direnv hook bash)"' > /Users/`whoami`/.bash_profile
+source ~/.bash_profile
+printf "${GREEN}✓✓✓ direnv installed!${NC}\n\n\n"
+
+printf "${BLUE}Installing Atom...${NC}\n"
+brew cask install atom
+printf "${GREEN}✓✓✓ Atom installed!${NC}\n\n\n"
+
+printf "${BLUE}Creating virtual environment...${NC}\n"
+python3 -m venv env
+echo 'source env/bin/activate' > .envrc
+direnv allow .
+printf "${GREEN}✓✓✓ virtual environment created! ${NC}\n\n\n"
+
+printf "${PURPLE}Your computer is configured! Please restart Terminal. ${NC}\n"
